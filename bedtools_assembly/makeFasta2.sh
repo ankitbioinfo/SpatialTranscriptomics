0e1881a669c64506de839af44501541dae92f9e4
@nkit <35502824+ankitbioinfo@users.noreply.github.com>
2021-12-22 16:35:45 +0100

    Delete makeFasta2.sh

--- bedtools_assembly/makeFasta2.sh
+++
@@ -1,4 +0,0 @@
-
-
-bedtools getfasta -fi Mus_musculus.GRCm39.dna.primary_assembly.fa -bed exon.dat -s -name -fo exon.fa
-bedtools getfasta -fi Mus_musculus.GRCm39.dna.primary_assembly.fa -bed intron.dat -s -name -fo intron.fa
