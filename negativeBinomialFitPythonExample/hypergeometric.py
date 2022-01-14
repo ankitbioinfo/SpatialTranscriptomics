91cac13e255dee1be973f420d000330d883a0971
@nkit <35502824+ankitbioinfo@users.noreply.github.com>
2022-01-14 11:08:26 +0100

    Create hypergeometric.py

---
+++ negativeBinomialFitPythonExample/hypergeometric.py
@@ -0,0 +1,36 @@
+from scipy.stats import hypergeom
+from scipy.stats import fisher_exact
+import numpy as np
+
+
+M=13
+n=8
+N=7
+x=3
+
+table=np.array([[x,n-x],[N-x,M-n-N+x]])
+#table = np.array([[6, 2], [1, 4]])
+#table = np.array([[15, 35], [135, 215]])
+
+M = table.sum()
+n = table[0].sum()
+N = table[:, 0].sum()
+start, end = hypergeom.support(M, n, N)
+#pb1=hypergeom.pmf(np.arange(start, end+1), M, n, N)
+
+xr=np.arange(min(n,N)+1)
+pb1=hypergeom.pmf(xr,M,n,N)
+
+print(M,n,N,xr,np.sum(pb1),sum(pb1[0:16]),sum(pb1[16:]))
+print(pb1)
+
+oddsr, p = fisher_exact(table, alternative='two-sided')
+print('two sideded',p)
+
+oddsr, p = fisher_exact(table, alternative='greater')
+print('greater',p)
+
+oddsr, p = fisher_exact(table, alternative='less')
+print('less',p)
+
+print(table)
