e7ce309e5d6fed2fb16d91fef82ca486b4984a4e
@nkit <35502824+ankitbioinfo@users.noreply.github.com>
2022-06-10 13:34:19 +0200

    Add files via upload

---
+++ Liver/downsample/create_downsample_in_sc_annotation.py
@@ -0,0 +1,37 @@
+
+
+
+
+import scanpy as sc
+import pandas as pd
+
+
+new_adata=sc.read_h5ad("sc_liver_data_downsample.h5ad")
+
+cellname=new_adata.obs_names.to_numpy()
+genename=new_adata.var_names.to_numpy()
+
+print('gene',len(genename))
+
+
+annot=pd.read_csv('./../annot_mouseStStAll.csv')
+data=annot.to_numpy()
+all_barcode_cell_name=data[:,5]
+print(all_barcode_cell_name[0:5])
+
+d={}
+for i in range(len(data)):
+    bid=data[i,5]
+    d[bid]=i
+
+
+
+index=[]
+for i in range(len(cellname)):
+    name=cellname[i]
+    index.append(d[name])
+    #print(key,dn[key],len(d[key]),count)
+
+
+df=pd.DataFrame(data=data[index],columns=annot.columns)
+df.to_csv('downsample_annotation.csv',index=False)
---
+++ Liver/downsample/spatial_downsample_in_csv.py
@@ -0,0 +1,40 @@
+
+
+
+import scanpy as sc
+import pandas as pd
+
+
+new_adata=sc.read_h5ad("spatial_quadrant_7489.h5ad")
+
+cellname=new_adata.obs_names.to_numpy()
+genename=new_adata.var_names.to_numpy()
+
+print('gene',len(genename))
+
+
+spatialdata=new_adata.copy()
+
+sc.pp.normalize_total(spatialdata, inplace=True)
+sc.pp.log1p(spatialdata)
+sc.pp.highly_variable_genes(spatialdata, flavor="seurat", n_top_genes=2000)
+#sc.pl.highly_variable_genes(full_ad_sc,show=False,save='.png')
+#newsc2 = newsc[:, newsc.var.highly_variable]
+
+new_adata=new_adata[:,spatialdata.var.highly_variable]
+mat=new_adata.X.toarray()
+print('2',len(spatialdata.var.highly_variable),mat.shape)
+
+
+df=pd.DataFrame(data=mat.transpose(), index=genename , columns=cellname)
+df.to_csv("spatial_liver_data_downsample_7489.csv")
+
+df=pd.read_csv("tissue_positions_list_7489.csv",header=None)
+data=df.to_numpy()
+print(data.shape,len(cellname))
+
+count=0
+for i in range(len(cellname)):
+    if cellname[i]==data[i,0]:
+        count+=1
+print(count)
